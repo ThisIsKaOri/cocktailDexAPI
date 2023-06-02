83c61baeca8dcc0bd2483b9de34cb0958afd3f18
thisiskaori <andreadieli@icloud.com>
2023-06-02 11:43:17 +0200

    feature(auth routes and test added)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 
+
 //importo le routes
+import { authRoutes } from "./routes/auth";
 import { ingredientsRoutes } from "./routes/ingredients";
 import { cocktailsRoutes } from "./routes/cocktails";
 
 export const app = express();
 
+//configuro CORS per effettuare le chiamate mentre sviluppo il front-end
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  next();
+});
+
 app.use(express.json());
 //definisco i path a cui saranno disponibili le mie routes
+app.use('/v1/auth', authRoutes);
 app.use('/v1/ingredients', ingredientsRoutes);
 app.use('/v1/cocktails', cocktailsRoutes);
 
